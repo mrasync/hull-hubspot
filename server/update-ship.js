660cda0552e6b587e2f0c0169a0bed43607bd6cb
Stephane Bellity <sbellity@gmail.com>
2016-08-31 14:32:45 +0200

    remove legacy code

--- server/update-ship.js
+++
@@ -1,7 +0,0 @@
-import Hubspot from "./lib/hubspot";
-
-export default function ({ clientID, clientSecret }, { message = {} }, { hull = {}, ship = {} }) {
-  const hubspot = new Hubspot({ clientID, clientSecret, ship, hull });
-  hubspot.syncHullGroup();
-  return true;
-}
