0ccc8844e1e5fef3d031f50bf7d7fca917d0e25c
Romain Dardour <romain@hull.io>
2016-09-08 10:37:03 +0200

    Serve assets folder

--- server/app/web-app.js
+++ server/app/web-app.js
@@ -1,14 +1,16 @@
 import express from "express";
+import path from "path";
 import { renderFile } from "ejs";
-
 import TokenMiddleware from "../lib/middleware/token";
 
 export default function () {
   const app = express();
 
+
   app.use(TokenMiddleware);
   app.engine("html", renderFile);
-  app.set("views", `${__dirname}/../../views`);
+  app.use(express.static(path.resolve(__dirname, "..", "assets")));
+  app.set("views", path.resolve(__dirname, "..", "..", "views"));
   app.set("view engine", "ejs");
 
   return app;
