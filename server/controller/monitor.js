31e0e97cbec08e4fe1b5a3d7c0e959c6bcd82f06
Stephane Bellity <sbellity@gmail.com>
2016-09-10 10:38:24 +0200

    we do not need to wait for the response here

--- server/controller/monitor.js
+++ server/controller/monitor.js
@@ -1,7 +1,7 @@
 export default class MonitorController {
 
   checkTokenAction(req, res) {
-    return req.shipApp.hubspotAgent.checkToken()
-      .then((status) => res.end(status));
+    req.shipApp.hubspotAgent.checkToken();
+    res.end(status);
   }
 }
